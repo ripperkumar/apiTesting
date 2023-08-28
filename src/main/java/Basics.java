c80e0447ce438a409c026900360dd01f82fc47b6
TestVagrant <testvagrant@Sudhindras-MacBook-Pro.local>
2023-08-28 12:34:17 +0530

    initial commit

---
+++ src/main/java/Basics.java
@@ -0,0 +1,12 @@
+import io.restassured.RestAssured;
+
+public class Basics {
+    public static void main(String[] args) {
+        //validate if Add place API is working or not
+        //given-all input details
+        //when- submit input details
+        //then - Validate input details
+        RestAssured.baseURI="https://rahulshettyacademy.com";
+
+    }
+}
