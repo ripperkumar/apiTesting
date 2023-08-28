eb6fe239ff150342546f424f99e9155fc3de6946
TestVagrant <testvagrant@Sudhindras-MacBook-Pro.local>
2023-08-28 16:03:18 +0530

    capturing response and parsing json

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,5 +1,6 @@
 import files.Payload;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
@@ -11,11 +12,18 @@ public class Basics {
         //given-all input details
         // when-  submit input details
         //then - Validate input details
-        RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").body(Payload.AddPlace())
-                .when().post("maps/api/place/add/json")
-                .then().log().all().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)");
 
         //add place-> update it with new address-> Get place to validate if new place is presentin response
+
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").body(Payload.AddPlace())
+                .when().post("maps/api/place/add/json")
+                .then().log().all().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)")
+                .extract().response().asString();
+
+        JsonPath js = new JsonPath(response);
+        String placeId = js.getString("place_id");
+        System.out.println(placeId);
+
     }
 }
