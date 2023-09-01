82e626daef2b2c44ae093700f387f9df4299b7be
TestVagrant <testvagrant@Sudhindras-MacBook-Pro.local>
2023-09-01 11:47:48 +0530

    added the new addbook2 method for getting dynamic payload from different json file

--- src/main/java/org/example/DynamicJson.java
+++ src/main/java/org/example/DynamicJson.java
@@ -6,6 +6,10 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.testng.annotations.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.*;
 
 public class DynamicJson {
@@ -15,6 +19,22 @@ public class DynamicJson {
         RestAssured.baseURI= "http://216.10.245.166";
         String response = given().log().all().header("Content-Type","application/json").
                 body(Payload.AddBooks(isbn,aisle)).
+                when().post("/Library/Addbook.php").
+                then().log().all().assertThat().statusCode(200).extract().response().asString();
+
+        JsonPath js = ReUsableMethods.rawToJson(response);
+        String id = js.getString("ID");
+        System.out.println(id);
+    }
+
+
+
+//for getting payload from external files
+    @Test (dataProvider="BooksData")
+    public void addBook2(String isbn,String aisle) throws IOException {
+        RestAssured.baseURI= "http://216.10.245.166";
+        String response = given().log().all().header("Content-Type","application/json").
+                body(new String(Files.readAllBytes(Paths.get("Give the path of the file")))).
                 when().post("/Library/Addbook.php").
                 then().log().all().assertThat().statusCode(200).extract().response().asString();
 
